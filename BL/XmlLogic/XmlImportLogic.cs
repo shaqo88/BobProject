57a9377e696facec2c886c78669b414fb65d1cf3
Shaul Royzen <shauldr@gmail.com>
2014-07-16 19:08:19 +0300

    Add XML import logic (not working yet)

---
+++ BL/XmlLogic/XmlImportLogic.cs
@@ -0,0 +1,31 @@
+﻿using BL.SchemaLogic.SchemaTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BL.XmlLogic
+{
+    public static class XmlImportLogic
+    {
+        public static XmlSchemaElementWrapper XmlDocumentToSchemaWrapper(XmlDocument xmlDoc)
+        {
+            
+            return null;
+        }
+
+        public static Version GetVersionOfXml(XmlDocument xmlDoc)
+        {
+            Version ver;
+
+            var dec = xmlDoc.ChildNodes.OfType<XmlDeclaration>().FirstOrDefault();
+
+            if (dec != null && Version.TryParse(dec.Version, out ver))
+                return ver;
+
+            return new Version(0, 0);
+        }
+    }
+}
